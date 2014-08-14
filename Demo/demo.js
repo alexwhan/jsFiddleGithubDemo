312e74feec715e908bc65d964c4085bd3f8f0836
alexwhan <alexwhan@gmail.com>
2014-08-14 17:02:05 +1000

    Update demo.js
    clunky attempt at tooltip modification

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,11 +1,16 @@
-window.addEvent('domready', function() {
-	new Request.HTML({
-		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-		data: {'delay': 1},
-		method: 'post',
-		update: 'demo',
-		onSuccess: function(response) {
-			$('demo').highlight();
-		}
-	}).send();
-})
+var data = [0];
+var svgContainer = d3.select('body').append('svg')
+                .attr('width', 200)
+                .attr('height', 200)
+    .attr('class', 'demo-tooltip');
+
+        var groups = svgContainer.selectAll('g')
+                .data(data)
+                .enter()
+                .append('g');
+
+        groups.append('circle')
+                .attr('cx', 100)
+                .attr('cy', 100)
+                .attr('r', 25)
+                .attr('fill', 'red');
